81e852e179f86a2e0477784f91b2415b7c024926
roberta soliman donofreo <47365108+robertasolimandonofreo@users.noreply.github.com>
2022-01-22 15:54:09 -0300

    -

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull robertasolimandonofreo/frontend:latest
-docker run -it -p 3000:3000 robertasolimandonofreo/frontend:latest
+docker run -p 3000:3000 robertasolimandonofreo/frontend:latest
