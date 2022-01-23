caa06242e23b3c26e37495b3bff4e2fb678b82c9
roberta soliman donofreo <47365108+robertasolimandonofreo@users.noreply.github.com>
2022-01-23 16:21:55 -0300

    -

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull robertasolimandonofreo/frontend:latest
-docker run -p 3000:3000 -e CHOKIDAR_USEPOLLING=true robertasolimandonofreo/frontend:latest tail -f /dev/null
+docker run -p 3000:3000 robertasolimandonofreo/frontend:latest
 docker start $(docker ps -q)
 
